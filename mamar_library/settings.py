f1e208b21cebec30d996c6ef6f03cd886a5d848c
salauddin85 <ahmedsalauddin677785@gmail.com>
2024-08-09 17:19:23 +0600

    dj database added

--- mamar_library/settings.py
+++ mamar_library/settings.py
@@ -110,7 +110,7 @@ WSGI_APPLICATION = 'mamar_library.wsgi.application'
 DATABASES = {
     'default': dj_database_url.config(
         # Replace this value with your local database's connection string.
-        default='postgresql://mamar_library_zhpf_user:0g7IJ7nrWRV5upbxuzHHDSdnPY7UfxQi@dpg-cq6nvciju9rs73eor5k0-a.oregon-postgres.render.com/mamar_library_zhpf',
+        default='postgresql://fashionfusion_shop_mn7y_user:7PH0VXVFFJKj2uWIPnEvko4QxDwApkho@dpg-cqqvi6o8fa8c73flvm9g-a.oregon-postgres.render.com/fashionfusion_shop_mn7y',
         
     )
 }
@@ -170,3 +170,4 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
